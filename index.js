925bc3097cdda6f47445c07e0b9d73f0f17c6122
Himel <shahriarhasan221@gmail.com>
2026-01-20 11:44:37 +0600

    added to git

---
+++ index.js
@@ -0,0 +1,150 @@
+// //Problem 1: Reverse a String
+
+// function reverseString(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return reversed;
+// }
+
+// // Example
+// console.log(reverseString("hello")); // "olleh"
+
+// // Problem 2: Count Vowels in a String
+// function countVowels(str) {
+//   let count = 0;
+//   let vowels = "aeiouAEIOU";
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// // Example
+// console.log(countVowels("programming")); // 3
+
+// Problem 3: Check for Palindrome
+
+// function isPalindrome(str) {
+//   let reversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+//   return str === reversed;
+// }
+
+// // Examples
+// console.log(isPalindrome("madam")); // true
+// console.log(isPalindrome("hello")); // false
+
+// // Problem 4: Find the Maximum Number
+// function findMax(arr) {
+//   let max = arr[0];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// // Example
+// console.log(findMax([5, 1, 9, 3])); // 9
+
+// Problem 5: Remove Duplicates from an Array
+
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+}
+
+// Example
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // [1, 2, 3, 4]
+
+// // Problem 6: Sum of All Numbers in an Array
+// function sumArray(arr) {
+//   let sum = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// }
+
+// // Example
+// console.log(sumArray([1, 2, 3, 4])); // 10
+
+// // Problem 7: Find Even Numbers in an Array
+
+// function findEvenNumbers(arr) {
+//   let evens = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       evens.push(arr[i]);
+//     }
+//   }
+//   return evens;
+// }
+
+// // Example
+// console.log(findEvenNumbers([1, 2, 3, 4, 5, 6])); // [2, 4, 6]
+
+// // Problem 8: Capitalize First Letter of Each Word
+// function capitalizeWords(str) {
+//   let words = str.split(" ");
+//   let result = [];
+
+//   for (let i = 0; i < words.length; i++) {
+//     let word = words[i];
+//     let capitalized = word[0].toUpperCase() + word.slice(1);
+//     result.push(capitalized);
+//   }
+//   return result.join(" ");
+// }
+
+// // Example
+// console.log(capitalizeWords("hello world")); // "Hello World"
+
+// // Problem 9: Find the Factorial of a Number
+
+// function factorial(num) {
+//   let result = 1;
+
+//   for (let i = 1; i <= num; i++) {
+//     result *= i;
+//   }
+//   return result;
+// }
+
+// // Example
+// console.log(factorial(5)); // 120
+
+// // Problem 10: PingPong Challenge
+// function pingPong() {
+//   for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("PingPong");
+//     } else if (i % 3 === 0) {
+//       console.log("Ping");
+//     } else if (i % 5 === 0) {
+//       console.log("Pong");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// // Run
+// pingPong();
